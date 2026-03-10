3517d1af2320a6750951eb792b9adab614eff8a0
Tobias <tobimation23@gmail.com>
2026-03-10 14:14:55 +0100

    impotere news og hero ind i homepagen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-import Image from "next/image";
-import SplashScreen from "./components/SplashScreen";
-
+import News from "./components/news/news";
+import Hero from "./components/hero/hero";
 export default function Home() {
+
   return (
     <>
-      <SplashScreen />
+   
       <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-      <h1>home page</h1>
+        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white">
+          <Hero />
+   <News />
         </main>
       </div>
     </>
