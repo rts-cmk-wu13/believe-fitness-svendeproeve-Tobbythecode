722b94a95029666a33bef7e91e481442ee0b530b
Tobias <tobimation23@gmail.com>
2026-03-10 14:38:33 +0100

    fiksede Hero

---
+++ src/app/components/hero/hero.jsx
@@ -0,0 +1,12 @@
+
+
+export default async function Hero(){
+
+
+
+  return(
+    <>
+<img className="w-full" src="mainHero.png" alt="" />
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
 
   return (
     <>
-   
+   <Hero />
       <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white">
-          <Hero />
+        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-10 px-16 bg-white">
+          
    <News />
         </main>
       </div>
