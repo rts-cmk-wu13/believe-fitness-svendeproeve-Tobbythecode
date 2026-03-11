daab27fa1e0098fc6e75ad078af0b3f0e9eeafc2
Tobias <tobimation23@gmail.com>
2026-03-11 14:49:27 +0100

    indsat Image og fjernede Image fra next i disse to

--- src/app/components/newscard/NewsCard.jsx
+++ src/app/components/newscard/NewsCard.jsx
@@ -1,5 +1,7 @@
 
 
+import Image from "next/image";
+
 export default function NewsCard({ news} ) {
 
 
@@ -12,7 +14,7 @@ export default function NewsCard({ news} ) {
         <h1 className="text-black text-3xl font-bold py-2">{news.title}</h1>
         <figure>
 
-    <img src={news.asset.url} alt="placeholder" />
+    <Image src={news.asset.url} alt="placeholder" width={800} height={400} />
 </figure>
      
         <p className="text-black text-xl py-5 font-extralight">{news.text}</p>
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,6 +1,4 @@
 import { cookies } from "next/headers";
-import Link from "next/link"
-import Image from "next/image";
 
 
 
