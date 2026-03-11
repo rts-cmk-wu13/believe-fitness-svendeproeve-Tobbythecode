0f6541285034c6e121f795a21340ac3444da5a5c
Tobias <tobimation23@gmail.com>
2026-03-11 15:01:14 +0100

    ændret på styling så det ser bedre ud

--- src/app/components/ClassCard/ClassCard.jsx
+++ src/app/components/ClassCard/ClassCard.jsx
@@ -6,16 +6,16 @@ export default function ClassCard({ class: classData }) {
       <main className="max-w-3xl flex flex-col items-center gap-4">
         <Link href={`/classes/${classData.id}`}>
           <article className="flex flex-col items-center gap-3">
-   <div className="relative w-full h-full rounded-lg overflow-hidden">
+   <div className="relative w-full sm:w-125 h-125 rounded-lg overflow-hidden">
               <img
-                className="w-full  h-full object-cover"
+                className="w-full h-full object-cover"
                 src={classData.asset.url}
                 alt={classData.className}
               />
 
            
-<div className="absolute bottom-0 pt-5 left-0 w-1/2 rounded-tr-full  bg-[rgb(241,196,14)] text-white p-4 text-left">
-  <h2 className="text-lg text-black font-bold">{classData.className}</h2>
+<div className="absolute bottom-0 left-0 w-full bg-[rgba(0,49,71,0.75)] text-white p-4 text-left">
+  <h2 className="text-lg font-semibold">{classData.className}</h2>
 </div>
             </div>
           
