39b55cd490dfc4c0043df032b40bdc6364fdd56e
Tobias <tobimation23@gmail.com>
2026-03-11 15:53:38 +0100

    fjernede image fra next da det ødeladge home

--- src/app/components/newscard/NewsCard.jsx
+++ src/app/components/newscard/NewsCard.jsx
@@ -1,7 +1,5 @@
 
 
-import Image from "next/image";
-
 export default function NewsCard({ news} ) {
 
 
@@ -14,7 +12,7 @@ export default function NewsCard({ news} ) {
         <h1 className="text-black text-3xl font-bold py-2">{news.title}</h1>
         <figure>
 
-    <Image src={news.asset.url} alt="placeholder" width={800} height={400} />
+    <img src={news.asset.url} alt="placeholder" />
 </figure>
      
         <p className="text-black text-xl py-5 font-extralight">{news.text}</p>
