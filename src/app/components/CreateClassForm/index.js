d7cfa1ad4ce58304ccbd5d6ed6f79509b7482c1c
Tobias <tobimation23@gmail.com>
2026-03-13 15:56:16 +0100

    ændring på meget af min inital states

--- src/app/components/CreateClassForm/index.js
+++ src/app/components/CreateClassForm/index.js
@@ -5,11 +5,12 @@ import { createClass } from "@/lib/dal";
 
 
 const initialState = {
-    name: null,
-    description: null,
-    weekday: null,
-    time: null,
+    className: null,
+    classDescription: null,
+classDay: null,
+    classTime: null,
     maxParticipants: null,
+    trainerId: null,
     asset: null,
 }
 
@@ -23,34 +24,34 @@ const [state, formAction, isPending] = useActionState(createClass, initialState)
         <h1 className="text-3xl text-bold mb-4 "> </h1>
 
 
-<form action={formAction}>
+<form action={formAction} enctype="multipart/form-data">
     <div className="formgroup flex flex-col">
 <label htmlFor="name" className="sr-only">navn</label>
-<input type="text" name="name" id="className"  placeholder="className" className="border border-gray-300 rounded px-3 py-2" />
+<input type="text" name="className" id="className"  placeholder="className" className="border border-gray-300 rounded px-3 py-2" />
 
     </div>
         <div className="formgroup flex flex-col">
 <label htmlFor="description" className="sr-only">description</label>
-<input type="description" name="description" id="classDescription"  placeholder="description" className="border border-gray-300 rounded px-3 py-2" />
+<input type="description" name="classDescription" id="classDescription"  placeholder="description" className="border border-gray-300 rounded px-3 py-2" />
 
     </div>
         <div className="formgroup flex flex-col">
-<label htmlFor="weekday" className="sr-only">class day</label>
-<select name="weekday"  id="weekday" className="border border-gray-300 rounded px-3 py-2">
+<label htmlFor="classDay" className="sr-only">class day</label>
+<select name="classDay"  id="classDay" className="border border-gray-300 rounded px-3 py-2">
 
-<option value="mandag"> mandag</option>
-<option value="tirsdag"> tirsdag</option>
-<option value="onsdag"> onsdag</option>
-<option value="torsdag"> torsdag</option>
-<option value="fredag"> fredag</option>
-<option value="lørdag"> lørdag</option>
-<option value="søndag"> søndag</option>
+<option value="Monday"> Monday</option>
+<option value="Tuesday"> Tuesday</option>
+<option value="Wednesday"> Wednesday</option>
+<option value="Thursday"> Thursday</option>
+<option value="Friday"> Friday</option>
+<option value="Saturday"> Saturday</option>
+<option value="Sunday"> Sunday</option>
 </select>
     </div>
 
         <div className="formgroup flex flex-col">
-<label htmlFor="time" className="sr-only">tid</label>
-<input type="text" name="time" id="classTime"   placeholder="tid" className="border border-gray-300 rounded px-3 py-2" />
+<label htmlFor="classTime" className="sr-only">tid</label>
+<input type="text" name="classTime" id="classTime"   placeholder="tid" className="border border-gray-300 rounded px-3 py-2" />
 
     </div>
 
@@ -58,22 +59,22 @@ const [state, formAction, isPending] = useActionState(createClass, initialState)
     
             <div className="formgroup flex flex-col">
 <label htmlFor="admin" className="sr-only">admin</label>
-<input type="text" name="admin" id="admin "readOnly value={`${userFirstName}} ${userLastName}`} placeholder="navn" className="border border-gray-300 rounded px-3 py-2" />
+<input type="text" id="trainerName" readOnly value={`${userFirstName} ${userLastName}`} placeholder="navn" className="border border-gray-300 rounded px-3 py-2" />
 
     </div>
  
         <div className="formgroup flex flex-col">
 <label htmlFor="maxParticipants" className="sr-only">max antal</label>
-<input type="text"  name=" maxParticipants" id="maxParticipants"  placeholder="navn" className="border border-gray-300 rounded px-3 py-2" />
+<input type="text"  name="maxParticipants" id="maxParticipants"  placeholder="maxParticipants" className="border border-gray-300 rounded px-3 py-2" />
 
     </div>
 
          <div className="formgroup flex flex-col">
-<label htmlFor="asset" className="sr-only">billede</label>
-<input type="file" id="asset" name="asset" placeholder="navn" className="border border-gray-300 rounded px-3 py-2" />
+<label htmlFor="file" className="sr-only">billede</label>
+<input type="file" id="file" name="file" accept="image/png, image/jpeg" placeholder="navn"  className="border border-gray-300 rounded px-3 py-2"  />
 
     </div>
-
+<input type="hidden" name="trainerId" value={userId} />
     <button type="submit" className="bg-[rgb(241,196,14)] disabled:bg-gray-400 px-4 py-2 rounded-3xl  w-[65%] text-black "
         > send</button>
     
